09e8e9b55c87218f23ad379a7648ee4591ce5b5e
Teijo Laine <teijo@aropupu.fi>
2015-07-27 17:35:18 +0300

    Show text that gets painted when 's' is pressed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-var content = <h1>Title</h1>;
+var CodeBox = React.createClass({
+    getInitialState() {
+        this.props.events.onValue((steps) => {
+            this.setState({step: steps});
+        });
+        return {
+            level: "Text that gets painted\n" +
+                   "when you press 's' on\n" +
+                   "keyboard.",
+            step: 0
+        };
+    },
+    render() {
+        var completed = this.state.level.substr(0, this.state.step);
+        var left = this.state.level.substr(this.state.step);
+        return (
+            <pre>
+                <span style={{color: "red"}}>{completed}</span>{left}
+            </pre>
+        );
+    }
+});
 
-React.render(content, document.getElementById("main"));
+var step = 0;
+var listener = new window.keypress.Listener();
+var input = new Bacon.Bus();
+
+React.render(<div><CodeBox events={input} /></div>, document.getElementById("main"));
+
+listener.simple_combo("s", () => {
+    input.push(++step);
+});
