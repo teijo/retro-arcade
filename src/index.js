cebf1b3f57d8116aaba3682b59afcf17b8d16949
Teijo Laine <teijo@aropupu.fi>
2015-07-31 16:56:22 +0300

    Highlight parts of code, WIP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 var CodeBox = React.createClass({
     getInitialState() {
-        var level = "Text that gets painted\n" +
-            "when you press 's' on\n" +
-            "keyboard.";
+        var level =
+           "var listener = new window.keypress.Listener();\n" +
+           "players.forEach(<<player => {\n" +
+           "    var step = 0;\n" +
+           "    listener.simple_combo(player.trigger, () => {\n" +
+           "        player.input.push(++step);\n" +
+           "    });\n" +
+           "}>>);";
         this.props.events.takeWhile((step) => step <= level.length).onValue((steps) => {
             this.setState({step: steps});
         });
@@ -13,6 +18,8 @@ var CodeBox = React.createClass({
     },
     render() {
         var level = this.state.level;
+        level = level.replace("<<", '<span style="color: blue;">');
+        level = level.replace(">>", '</span>');
         var completed = level.substr(0, this.state.step);
         var cursor = level.substr(this.state.step, 1);
         var left = level.substr(this.state.step + 1);
@@ -22,7 +29,7 @@ var CodeBox = React.createClass({
             <div>
                 <h2>{this.props.name}</h2>
                 <pre>
-                    <span style={{color: "red"}}>{completed}</span><span style={{backgroundColor: "lime"}}>{cursor}</span>{left}
+                    <span style={{color: "red"}}>{completed}</span><span style={{backgroundColor: "lime"}}>{cursor}</span><span dangerouslySetInnerHTML={{__html: left}} />
                 </pre>
                 <h3>Progress: {progress.toFixed(2)}%</h3>
             </div>
