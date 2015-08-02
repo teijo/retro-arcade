cd752d29292d239977e71716afc14bcda1645045
Teijo Laine <teijo@aropupu.fi>
2015-08-02 14:54:42 +0300

    Special block traversal

--- src/index.js
+++ src/index.js
@@ -21,21 +21,42 @@ var CodeBox = React.createClass({
             specialsLeft: 3
         };
     },
+    getPosition(blocks, step) {
+        var blockIndex = -1;
+        var next = step;
+        var remainder;
+        do {
+            remainder = next;
+            blockIndex++;
+            next -= blocks[blockIndex].length;
+        } while(next > 0);
+        return [blockIndex, remainder];
+    },
     render() {
         var level = this.state.level;
-        level = level.replace(/<</g, '<span style="color: blue;">');
-        level = level.replace(/>>/g, '</span>');
+        var blocks = level.split(/<<|>>/);
         var step = this.state.step;
-        var completed = level.substr(0, step);
-        var cursor = level.substr(step, 1);
-        var left = level.substr(step + 1);
-        var length = level.length;
-        var progress = step / length * 100;
+        var [blockIndex, blockPosition] = this.getPosition(blocks, step);
+        var elements = blocks.map((block, index) => {
+            var baseColor = index % 2 == 0 ? "black" : "blue";
+            var key = "block_"+index;
+            if (index == blockIndex) {
+                var completed = block.substr(0, blockPosition);
+                var cursor = block.substr(blockPosition, 1);
+                var left = block.substr(blockPosition + 1);
+                return <span key={key} style={{color: baseColor}}><span style={{color: "red"}}>{completed}</span><span style={{backgroundColor: "lime"}}>{cursor}</span><span dangerouslySetInnerHTML={{__html: left}} /></span>;
+            } else if (index < blockIndex) {
+                return <span key={key} style={{color: "red"}}>{block}</span>
+            } else {
+                return <span key={key} style={{color: baseColor}}>{block}</span>;
+            }
+        });
+        var progress = step / level.length * 100;
         return (
             <div>
                 <h2>{this.props.name}</h2>
                 <pre>
-                    <span style={{color: "red"}}>{completed}</span><span style={{backgroundColor: "lime"}}>{cursor}</span><span dangerouslySetInnerHTML={{__html: left}} />
+                    {elements}
                 </pre>
                 <div className="row">
                     <div className="col-xs-3">Progress: {progress.toFixed(2)}%</div>
@@ -64,8 +85,8 @@ var players = [
 
 React.render(
     <div className="row">
-        {players.map(p => {
-            return <div className="col-xs-5">
+        {players.map((p, i) => {
+            return <div key={"player_" + i} className="col-xs-5">
                 <CodeBox name={p.name} events={p.input} />
             </div>
         })}
