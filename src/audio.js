07ad96766506c908e853dc0890ced3621389d9dc
Niko Kettunen <niko.kettunen@reaktor.fi>
2018-05-01 17:46:54 +0300

    Remove need for http-server
    Static web site for easier distribution

--- src/audio.js
+++ src/audio.js
@@ -1,26 +1,14 @@
 import { Howl } from "howler";
 
-const files = [
-  {name: "game", src: "assets/game.mp3", isBackgroundMusic: true},
-  {name: "menu", src: "assets/menu.mp3", isBackgroundMusic: true},
-  {name: "menuPickSfx", src: "assets/menu-pick.wav", isBackgroundMusic: false},
-  {name: "menuSwitchSfx", src: "assets/menu-switch.wav", isBackgroundMusic: false},
-  {name: "typeSfx", src: "assets/type.wav", isBackgroundMusic: false},
-  {name: "perfectSfx", src: "assets/perfect.wav", isBackgroundMusic: false},
-  {name: "autocompleteSfx", src: "assets/autocomplete.wav", isBackgroundMusic: false},
-  {name: "missSfx", src: "assets/miss.wav", isBackgroundMusic: false},
-  {name: "finishSfx", src: "assets/finish.wav", isBackgroundMusic: false}
-];
-
 export const loadAudioContext = () => {
-  function player(file) {
+  function player(audioEntry) {
     const audio = new Howl({
-      src: [file.src],
-      loop: file.isBackgroundMusic
+      src: [audioEntry.src],
+      loop: audioEntry.isBackgroundMusic
     });
 
     const context = {};
-    context[file.name] = {
+    context[audioEntry.name] = {
       loop(active) {
         audio.loop(true);
         if (active === true) {
@@ -43,7 +31,7 @@ export const loadAudioContext = () => {
   }
 
   // Return a playback functions for each loaded file
-  return files
-    .map((fileName) => player(fileName))
+  return window.audioData
+    .map(player)
     .reduce((memo, current) => Object.assign(current, memo, {}), {});
 };
